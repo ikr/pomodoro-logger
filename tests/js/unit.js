7969ced18d06432e237745bf85224cb41f204b56
Ivan Krechetov <ikr@xiag.ch>
2010-08-20 10:48:04 +0200

    Container element in the HTML testing harness

--- tests/js/unit.js
+++ tests/js/unit.js
@@ -1,4 +1,14 @@
+module("GUI");
+
 test("HTML GUI application version is defined", function() {
     equals(typeof pomodoroLoggerApplication.version, "string", "Application version is a string");
     ok(pomodoroLoggerApplication.version.length > 0, "Application version string isn't empty");
+});
+
+test("The GUI container element exists in the test harness", function() {
+    equals($('#test_container').size(), 1);
+});
+
+test("The GUI container in the test harness is invisible", function() {
+    equals($('#test_container').css("display"), "none");
 });
