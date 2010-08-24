83ce98c50c7d8845469981dae09afaf887ac9031
Ivan Krechetov <ivan.krechetov@gmail.com>
2010-08-24 10:28:44 +0200

    Minimal marketable version :)

--- src/js/html-ui.js
+++ src/js/html-ui.js
@@ -1,14 +1,4 @@
-var pomodoroLoggerApplication = function($) {
-    function appendGuiContent(containerSelector) {
-        $(containerSelector).append('<div id="checkMarks"/>');
-        $(containerSelector).append('<div id="lastMarkTime"/>');
-        $(containerSelector).append('<button id="doneButton" type="button">Done</button>');
-    }
-    
-    function bindDoneButtonClickHandler() {
-        
-    }
-    
+var pomodoroLoggerApplication = function($, clock) {
     return {
         version: "0.0.1",
         
@@ -16,5 +6,36 @@ var pomodoroLoggerApplication = function($) {
             appendGuiContent(containerSelector);
             bindDoneButtonClickHandler();
         }
+    }
+    
+    function appendGuiContent(containerSelector) {
+        $(containerSelector).append('<div id="checkMarks"/>');
+        $(containerSelector).append('<div>Total: <span id="totalCount">0</span></div>');
+        $(containerSelector).append('<div>Last: <span id="lastMarkTime"></span></div>');
+        $(containerSelector).append('<button id="doneButton" type="button">Done</button>');
+    }
+    
+    function bindDoneButtonClickHandler() {
+        $("#doneButton").click(function() {
+            $("#checkMarks").append("x");
+            incrementElementInnerValue("#totalCount");
+            $("#lastMarkTime").empty().append(theClock().now());
+        });
+    }
+    
+    function incrementElementInnerValue(elementSelector) {
+        var currentValue = parseInt($(elementSelector).html());
+        $(elementSelector).empty();
+        $(elementSelector).append(currentValue + 1);
+    }
+    
+    function theClock() {
+        if ('undefined' === typeof clock) return {
+            now: function() {
+                return (new Date()).toTimeString();
+            }
+        }
+        
+        return clock;
     }
 }
--- tests/js/unit.js
+++ tests/js/unit.js
@@ -1,4 +1,4 @@
-module("Test harness")
+module("Meta: test harness itself")
 
 test("The GUI container element exists in the test harness", function() {
     equals($("#testContainer").size(), 1);
@@ -11,18 +11,26 @@ test("The GUI container in the test harness is invisible", function() {
 //==================================================================================================
 
 module(
-    "GUI",
+    "GUI basics",
     {
         setup: function() {
             $('#testContainer').empty();
-            pomodoroLoggerApplication($).init("#testContainer");
+            pomodoroLoggerApplication($, theTimeIsNowClock()).init("#testContainer");
         }
     }
 );
 
 test("HTML GUI application version is defined", function() {
-    equals(typeof pomodoroLoggerApplication($).version, "string", "Application version is a string");
-    ok(pomodoroLoggerApplication($).version.length > 0, "Application version string isn't empty");
+    equals(
+        typeof pomodoroLoggerApplication($, theTimeIsNowClock()).version,
+        "string",
+        "Application version is a string"
+    );
+    
+    ok(
+        pomodoroLoggerApplication($, theTimeIsNowClock()).version.length > 0,
+        "Application version string isn't empty"
+    );
 });
 
 test("On init adds a button to the specified container", function() {
@@ -42,12 +50,61 @@ test("On init adds the check marks' div", function() {
     ok($("#testContainer > #checkMarks").is("div"));
 });
 
-test("On init adds the last mark time div", function() {
-    equals($("#testContainer > #lastMarkTime").size(), 1);
-    ok($("#testContainer > #lastMarkTime").is("div"));
+test("On init adds an empty last mark time span", function() {
+    equals($("#testContainer #lastMarkTime").size(), 1);
+    ok($("#testContainer #lastMarkTime").is("span"), "Timestamp must be in a span");
+    equals($("#testContainer #lastMarkTime").html(), "");
 });
 
 test("Clicking Done adds a check mark", function() {
     $("#testContainer > button").trigger("click");
     equals($("#checkMarks").html(), "x");
+    
+    $("#testContainer > button").trigger("click");
+    equals($("#checkMarks").html(), "xx");
 });
+
+test("Initial total count is zero", function() {
+    equals($("#testContainer #totalCount").html(), "0");
+});
+
+test("Clicking Done increments the total count", function() {
+    $("#testContainer > button").trigger("click");
+    equals($("#totalCount").html(), "1");
+    
+    $("#testContainer > button").trigger("click");
+    equals($("#totalCount").html(), "2");
+});
+
+//==================================================================================================
+
+module(
+    "GUI timing",
+    {
+        setup: function() {
+            $('#testContainer').empty();
+        }
+    }
+);
+
+test("On Done click the current timestamp is printed", function() {
+    pomodoroLoggerApplication($, theTimeIsNowClock()).init("#testContainer");
+    
+    $("#testContainer > button").trigger("click");
+    
+    equals($("#lastMarkTime").html(), "The time is now");
+});
+
+test("Done click handling works without explicit clock passed", function() {
+    pomodoroLoggerApplication($).init("#testContainer");
+    $("#testContainer > button").trigger("click");
+    ok($("#lastMarkTime").html().length > 0, "Timestamp must be not empty");
+});
+
+//==================================================================================================
+
+function theTimeIsNowClock() {
+    return {
+        now: function() { return "The time is now"; }
+    };
+}
